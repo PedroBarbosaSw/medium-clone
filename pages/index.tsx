47febd15c2c9ef84f813f0c9894316c7f8842417
Pedro Barbosa <pedro_dith@hotmail.com>
2022-07-01 19:31:31 -0300

    showing posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
-import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
 
-const Home: NextPage = () => {
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
+
+interface Props {
+  posts: [Post];
+}
+
+const Home = ({ posts }: Props) => {
+  console.log(posts);
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -21,7 +29,7 @@ const Home: NextPage = () => {
             is a place to write, read, and connect
           </h1>
           <h2>
-            It's easy and free to post hour thinking no any topic and connect
+            It's easy and free to post our thinking on any topic and connect
             with millions of readers
           </h2>
         </div>
@@ -32,8 +40,59 @@ const Home: NextPage = () => {
           alt="a big m"
         />
       </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt="post picture"
+              />
+
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt="author picture"
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"] {
+    _id,
+    title,
+    author -> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
